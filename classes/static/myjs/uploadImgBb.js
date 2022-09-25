107645595bf2d2ec8a5f194c57c190e155e06111
RAHUL-PC\Rzope <rzope5923@gmail.com>
2022-09-25 18:13:00 +0530

    Upload image using Automated API using api.imgbb.com

---
+++ classes/static/myjs/uploadImgBb.js
@@ -0,0 +1,19 @@
+function uploadFile() {
+  var file = document.getElementById("fileOb");
+  var form = new FormData();
+  form.append("image", file.files[0]);
+  var inputs = {
+    url: "https://api.imgbb.com/1/upload?key=e558782bf47c31c13148b390a996743e",
+    method: "POST",
+    timeout: 0,
+    processData: false,
+    mimeType: "multipart/form-data",
+    contentType: false,
+    data: form,
+  };
+
+  $.ajax(inputs).done(function (response) {
+    var job = JSON.parse(response);
+    $("#imageUrl").val(job.data.url);
+  });
+}
